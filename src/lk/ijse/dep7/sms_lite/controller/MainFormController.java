f026f9b26b77d335217c50ff4cf9714fc3eac7f2
Aruni Dissanayake <aruni.saku@gmail.com>
2021-08-02 06:24:19 +0530

    add lib folder

--- src/lk/ijse/dep7/sms_lite/controller/MainFormController.java
+++ src/lk/ijse/dep7/sms_lite/controller/MainFormController.java
@@ -1,7 +1,31 @@
 package lk.ijse.dep7.sms_lite.controller;
 
-public class MainFormController {
-    public static void main(String[] args) {
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
+public class MainFormController {
+    public TextField txtID;
+    public TextField txtName;
+    public TextField txtPhone;
+    public ListView lstPhone;
+    public TableView tblStudent;
+    public Button btnClear;
+    public Button btnRemove;
+    public Button btnSave;
+
+    public void initialize(){
+
+    }
+
+    public void btnClear_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnRemove_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
     }
 }
